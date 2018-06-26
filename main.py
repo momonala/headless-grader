b0ee66a3d4c7b58bf543db68c47e269c55817797
momonala <mnalavadi@gmail.com>
2018-06-26 19:56:03 +0200

    add scheduler and headless implementation error

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from grader import launch_browser, Grader
+import schedule
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--headless', default=False)
 args = parser.parse_args()
 
-# start a session to grade a project!
-browser = launch_browser(headless=args.headless)
+if args.headless:
+    raise NotImplementedError('Headless mode not working - cntrl+C unimplemented')
 
-headless_grader = Grader(browser)
-headless_grader.login()
-headless_grader.refresh_queue()
 
-if headless_grader.get_project():
-    headless_grader.grade_project()
+def grade():
+    # start a session to grade a project!
+    browser = launch_browser(headless=args.headless)
 
-headless_grader.SLEEP(2)
-headless_grader.browser.quit()
+    headless_grader = Grader(browser)
+    headless_grader.login()
+    headless_grader.refresh_queue()
+
+    if headless_grader.get_project():
+        headless_grader.grade_project(log=True)
+
+    headless_grader.SLEEP(2)
+    headless_grader.browser.quit()
+
+
+schedule.every(1).minutes.do(grade)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
